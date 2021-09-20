0f8fd6de389429cf4c1453911c056d8338c10c8c
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-09-20 12:03:51 -0300

    Agruegue resolucion de un ejercicio

---
+++ Ejercicio practico-detalle de libro.py
@@ -0,0 +1,7 @@
+#Es necesario pedir el titulo y el autor
+#preguntarle al usuario los datos
+#devolve el valor del usuario
+
+titulo = input('Cual es el titulo del libro ?')
+autor = input('Quien fue el autor del libro ?')
+print(titulo, 'fue escrito por', autor)
