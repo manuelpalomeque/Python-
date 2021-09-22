1b0e4a1ec81b2430e988228d87f425f6a78fc2f3
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-09-22 18:24:42 -0300

    Incorpore mas practicos

--- Practica -Parte 2.py
+++ Practica -Parte 2.py
@@ -8,4 +8,12 @@ while contador <= 250:
 else:
     print('Fin del ciclo ')
 
+# iterar un rango de 0 a 10 e imprimir numeros divisibles
+# entre 3
+
+for i in range(10):
+    if i % 3 == 0:
+        print(i)
+else:
+    print('Fin del ciclo')
 
