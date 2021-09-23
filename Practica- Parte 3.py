6844ad003e646e69f9c34ca599f3a741aa8c2e9e
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-09-23 11:57:46 -0300

    Reubique el ejercicio. Por el momento, el archivo esta vacio

--- Practica- Parte 3.py
+++ Practica- Parte 3.py
@@ -1,6 +1,1 @@
-#Califica tu dia del  1 al  10
-#preguntarle al usuario como estuvo su dia
-#devolve el valor del usuario
-
-resultadoDia = int(input('Del 1 al 10, Como estuvo tu dia ?'))
-print('Mi dia estuvo de :', resultadoDia)
+#EN PROCESO
