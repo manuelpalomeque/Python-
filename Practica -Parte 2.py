f755411e2f7e5afdb9351cf620a33f51eef64836
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-09-27 11:26:55 -0300

    Resolvi ejercicios de ciclo For.

--- Practica -Parte 2.py
+++ Practica -Parte 2.py
@@ -1,3 +1,25 @@
+#LISTAS     ----------------------------------------------------------------------------
+
+# Transformar cada numero multiplicandolo por 2
+
+# Filtrar las comidas que no sean carne
+# Encuentra y devuelve la gallina
+# Dime donde esta la gallina
+# Rellenar la lista de dinero
+# esta bien?
+# Hay algun error?
+
+#TUPLAS     ----------------------------------------------------------------------------
+
+# Hacer un programa para ingresar cinco números distintos y luego mostrar por
+# pantalla el mayor y el menor de ellos.
+
+# Hacer un programa para ingresar cuatro números y luego mostrar por pantalla
+# cuáles son mayores a 100.
+
+# Hacer un programa para ingresar cuatro números y luego mostrar por pantalla
+# cuántos son menores a 100.
+
 #CICLO WHILE    ---------------------------------------------------------------------------
 #aumentar valor de  2 en 2, hasta llegar al  250:
 
@@ -8,7 +30,16 @@ while contador <= 250:
     contador += 2
 else:
     print('Fin del ciclo ')
+
 #CICLO FOR  -----------------------------------------------------------------------------
+# Hacer un programa para mostrar los números del 1 al 10. No se debe realizar
+# ningún pedido de datos.
+contador1 = 0
+
+for x in range(10):
+    contador1 += 1
+    print(contador1)
+
 # iterar un rango de 0 a 10 e imprimir numeros divisibles
 # entre 3
 
@@ -21,12 +52,33 @@ else:
 # Hacer un programa que solicite el ingreso de 10 números y que muestre el
 # mayor de ellos por pantalla. Solo se debe emitir UN valor por pantalla.
 
+numeros = []
+valor = None
+numeroMayor = 0
+
+for x in range(10):
+    valor = int(input('Ingrese el valor: '))
+    numeros.append(valor)
+    if valor > numeroMayor:
+        numeroMayor = valor
+else:
+    print(f'Él numero mas alto es {numeroMayor}, fin del ciclo')
+
 # Hacer un programa que solicite 20 números y calcule y emita por pantalla
 # cuántos son positivos (mayores a cero). Se debe mostrar un solo valor: el
 # conteo final.
 
-# Hacer un programa para mostrar los números del 1 al 10. No se debe realizar
-# ningún pedido de datos.
+numerosEvaluar = []
+contadorPositivos = 0
+valor1 = None
+
+for x in range(20):
+    valor1 = int(input('Ingrese el valor: '))
+    numerosEvaluar.append(valor1)
+    if valor1 > 0:
+        contadorPositivos += 1
+else:
+    print(f'La cantidad de numeros positivos son: {contadorPositivos}')
 
 # Hacer un programa para mostrar los números del 10 al 1. No se debe realizar
 # ningún pedido de datos.
@@ -54,26 +106,3 @@ else:
 # Hacer un programa para ingresar 10 números y luego calcule y emita el mayor
 # de los primos de la lista. En caso de no haber ningún número primo, deberá
 # aclararlo con un cartel.
-
-#LISTAS     ----------------------------------------------------------------------------
-
-# Transformar cada numero multiplicandolo por 2
-
-# Filtrar las comidas que no sean carne
-# Encuentra y devuelve la gallina
-# Dime donde esta la gallina
-# Rellenar la lista de dinero
-# esta bien?
-# Hay algun error?
-
-#TUPLAS     ----------------------------------------------------------------------------
-
-# Hacer un programa para ingresar cinco números distintos y luego mostrar por
-# pantalla el mayor y el menor de ellos.
-
-# Hacer un programa para ingresar cuatro números y luego mostrar por pantalla
-# cuáles son mayores a 100.
-
-# Hacer un programa para ingresar cuatro números y luego mostrar por pantalla
-# cuántos son menores a 100.
-
