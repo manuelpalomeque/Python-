16eaacea89b2a0c2d3cabe9adf762bb73f3685d6
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-09-27 10:50:16 -0300

    Agregue consignas

--- Practica -Parte 2.py
+++ Practica -Parte 2.py
@@ -18,8 +18,53 @@ for i in range(10):
 else:
     print('Fin del ciclo')
 
+# Hacer un programa que solicite el ingreso de 10 números y que muestre el
+# mayor de ellos por pantalla. Solo se debe emitir UN valor por pantalla.
+
+# Hacer un programa que solicite 20 números y calcule y emita por pantalla
+# cuántos son positivos (mayores a cero). Se debe mostrar un solo valor: el
+# conteo final.
+
+# Hacer un programa para mostrar los números del 1 al 10. No se debe realizar
+# ningún pedido de datos.
+
+# Hacer un programa para mostrar los números del 10 al 1. No se debe realizar
+# ningún pedido de datos.
+
+# Hacer un programa que muestre los números del 1 al 100 de 5 en 5. Ejemplo:
+# 0, 5, 10, 15, 20.... 100.
+
+# Hacer un programa que solicite UN número y luego calcule y emita un cartel
+# aclaratorio si el mismo es primo o no es primo.
+# Nota: un numero es primo cuando es divisible únicamente por 1 y por sí
+# mismo.
+
+# Hacer un programa que solicite 10 números y luego mostrar por pantalla el
+# máximo de ellos y la posición en la que fue ingresado.
+
+# Hacer un programa que solicite 20 números y luego mostrar por pantalla el
+# menor de ellos y la posición en la que fue encontrado.
+
+# Hacer un programa que solicite 20 edades y luego calcule el promedio de edad
+# de aquellas personas mayores a 18 años.
+
+# Hacer un programa que solicite 20 números y luego emitir por pantalla el
+# máximo de los números pares y el mínimo de los números impares.
+
+# Hacer un programa para ingresar 10 números y luego calcule y emita el mayor
+# de los primos de la lista. En caso de no haber ningún número primo, deberá
+# aclararlo con un cartel.
+
 #LISTAS     ----------------------------------------------------------------------------
 
+# Transformar cada numero multiplicandolo por 2
+
+# Filtrar las comidas que no sean carne
+# Encuentra y devuelve la gallina
+# Dime donde esta la gallina
+# Rellenar la lista de dinero
+# esta bien?
+# Hay algun error?
 
 #TUPLAS     ----------------------------------------------------------------------------
 
