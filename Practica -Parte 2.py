c0212c3481af6508936664e9ebdd073ac308c5de
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-09-30 12:34:18 -0300

    Resolvi un ejercicio y agregue mas consignas para resolver de ciclo While

--- Practica -Parte 2.py
+++ Practica -Parte 2.py
@@ -50,6 +50,44 @@ while cont <= 100:
 else:
     print('Fin del ciclo')
 
+# Hacer un programa para mostrar los números del 1 al 10. No se debe realizar
+# ningún pedido de datos. USANDO WHILE.
+
+# Hacer un programa para mostrar los números del 10 al 1. No se debe realizar
+# ningún pedido de datos. USANDO WHILE.
+
+# Hacer un programa que solicite la edad de un grupo de personas. El programa
+# deberá pedir edades hasta que se ingrese una edad menor a 18 años. Deberá
+# mostrar por pantalla cuántas personas mayores se registraron.
+
+# Hacer un programa que solicite dos números y luego muestre los números
+# entre el menor y el mayor de ellos. Acordate, usando While.
+
+# Hacer un programa que muestre los números del 1 al 100 de 5 en 5. Ejemplo:
+# 0, 5, 10, 15, 20.... 100. Usando While.
+
+# Hacer un programa que solicite UN número y luego calcule y emita un cartel
+# aclaratorio si el mismo es primo o no es primo.
+# Nota: usando While. Ya lo hicimos con FOR, ahora con While.
+
+# Hacer un programa que solicite una lista de números que corta cuando se
+# ingresa un cero y luego mostrar por pantalla el máximo de ellos y la posición
+# en la que fue ingresado.
+
+# Hacer un programa que solicite una lista de números que corta cuando se
+# ingresa un cero y luego mostrar por pantalla el menor y el segundo menor.
+
+# Realizar el nuevamente el ejercicio 8 pero ahora debe devolver además la
+# posición en la que fue encontrado cada uno de los mínimos.
+
+# Hacer un programa que solicite una lista de números que corta cuando se
+# ingresa un cero y luego emitir por pantalla el máximo de los números
+# negativos y el mínimo de los números positivos.
+
+# Hacer un programa para ingresar una lista de números que corta cuando se
+# ingresa un cero y luego mostrar: la cantidad de números primos, la cantidad de
+# números pares, la cantidad de positivos y la cantidad de negativos.
+
 #CICLO FOR  -----------------------------------------------------------------------------
 # Hacer un programa para mostrar los números del 1 al 10. No se debe realizar
 # ningún pedido de datos.
@@ -177,8 +215,3 @@ for m in range(20):
             minImpar_ = parOImpar
 
 print(f' El maximo de los numeros pares es {maxPar_} y el minimo de los impares es {minImpar_}')
-
-
-# Hacer un programa para ingresar 10 números y luego calcule y emita el mayor
-# de los primos de la lista. En caso de no haber ningún número primo, deberá
-# aclararlo con un cartel.
