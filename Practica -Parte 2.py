77f44f09c7c75bc923b094dc582b77a02d22bd86
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-10-04 11:12:36 -0300

    Resolvi ejercicios de Funciones

--- Practica -Parte 2.py
+++ Practica -Parte 2.py
@@ -334,6 +334,22 @@ print(res_)
 # Hacer una función llamada “par” que reciba un número entero y devuelva 1 si
 # es par o cero si no lo es. Hacer un programa para ingresar 20 números y
 # mostrar por pantalla cuántos son pares.
+def par(number_123):
+    if number_123 % 2 == 0:
+        return 1
+    else:
+        return 0
+
+contador_dePares = 0
+numeroQuePido = int(input('Ingrese el valor numerico: '))
+
+for g in range(20):
+    if par(numeroQuePido) == 1:
+        contador_dePares += 1
+    g += 1
+    numeroQuePido = int(input('Ingrese el valor numerico: '))
+else:
+    print(f'La cantidad de numeros pares es: {contador_dePares}')
 
 # Hacer una función llamada “primo” que reciba un número entero y devuelva 1
 # si el número es primo o cero si no lo es. Hacer un programa para ingresar
