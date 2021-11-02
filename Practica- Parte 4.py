d646b44404a495a2284e15d1f0574a22ea10c24e
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-02 15:14:02 -0300

    Agregue ejercicios de Clases

--- Practica- Parte 4.py
+++ Practica- Parte 4.py
@@ -1,3 +1,29 @@
 # CLASES   --------------------------------------------------------------------------------------
 
+# Crear una clase para realizar operaciones de sumar, restar, etc.
+class Aritmetica:
+
+    def __init__(self, operandoA, operandoB):
+        self.operandoA = operandoA
+        self.operandoB = operandoB
+
+    def sumar_3(self):
+        return  self.operandoA + self.operandoB
+
+    def restar_3(self):
+        return  self.operandoA - self.operandoB
+
+    def multiplicar_3(self):
+        return  self.operandoB * self.operandoB
+
+    def dividir_3(self):
+        return  self.operandoA / self.operandoB
+
+aritmetica1 = Aritmetica(4, 5)
+
+print(f'El resultado de la suma es {aritmetica1.sumar_3()}')
+print(f'El resultado de la resta es {aritmetica1.restar_3()}')
+print(f'El resultado de la multiplicacion es {aritmetica1.multiplicar_3()}')
+print(f'El resultado de la dicision es {aritmetica1.dividir_3():.2f}')
+
 # xxxxxxxxxxxxxxxxXX   ---------------------------------------------------------------------------
