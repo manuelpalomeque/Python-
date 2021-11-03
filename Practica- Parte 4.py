ca7b84bda9f926709a916e020f237087bdcdc11b
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-03 10:24:29 -0300

    Agregue ejercicios de Clases

--- Practica- Parte 4.py
+++ Practica- Parte 4.py
@@ -26,4 +26,20 @@ print(f'El resultado de la resta es {aritmetica1.restar_3()}')
 print(f'El resultado de la multiplicacion es {aritmetica1.multiplicar_3()}')
 print(f'El resultado de la dicision es {aritmetica1.dividir_3():.2f}')
 
+# Crear una clase para calcular el área de un rectángulo.
+class Rectangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def calcular_area(self):
+        return  self.base * self.altura
+
+base = float(input('Por favor ingrese la dimension de la base: '))
+altura = float(input('Por favor ingrese el valor de la altura: '))
+
+rectangulo1 = Rectangulo(base, altura)
+
+print(f'El área del rectángulo es: {rectangulo1.calcular_area():.2f} m2')
+
 # xxxxxxxxxxxxxxxxXX   ---------------------------------------------------------------------------
