a214e88d44be346efea4fb0d4f0b954334000791
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-05 11:41:28 -0300

    Agregue la clase hija Coche

--- Practica- Parte 4.py
+++ Practica- Parte 4.py
@@ -172,4 +172,21 @@ class Vehiculo:
     def __str__(self):
         return f'Color: {self.color}, ruedas: {self.ruedas}'
 
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self._velocidad = velocidad
+
+    @property
+    def velocidad(self):
+        return self._velocidad
+
+    @velocidad.setter
+    def velocidad(self, velocidad):
+        self._velocidad = velocidad
+
+    def __str__(self):
+        return f'''Coche:
+        {super().__str__()}, velocidad: {self._velocidad} km/hr'''
+
 # xxxxxxxxxxxxxxxxXX   ---------------------------------------------------------------------------
